1bb895c223204bf52e6cf4f322df782cf4f13fcb
smiritimis2 <113242412+smiritimis2@users.noreply.github.com>
2023-01-07 19:53:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,70 @@ let arr = [
 
 function PrintDeveloperswithMap() {
   //Write your code here
+  
+  var newarr=arr.map(item=>{if(item.profession==="developer")return item;})
+    console.log(newarr);
+
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here
+     arr.forEach((item)=>{if(item.profession==="developer") console.log(item);})
 }
 
 function addData() {
   //Write your code here
+  let len=arr.length;
+  
+
+    let arr2={
+     id:len,
+     name:'susan',
+     age:'20',
+     profession:'intern'
+    }
+    arr.push(arr2);
+    /*name.value="";
+    age.value="";
+    profession.value="";*/
+
+
+
+
+
+
+
+
+console.log(arr);
+
+
+
 }
 
 function removeAdmin() {
   //Write your code here
+  
+   let brr=arr.filter((item)=>{
+      if(item.profession==="developer")
+      {
+        return item;
+      }
+    })
+   console.log(brr);
+   
+        
 }
 
 function concatenateArray() {
   //Write your code here
+  let brr=[
+    { id: 7, name: "jam", age: "18", profession: "developer" },
+    { id: 8, name: "jck", age: "20", profession: "developer" },
+    { id: 9, name: "ren", age: "19", profession: "admin" },
+     
+   ]
+   //arr.concat(arr,brr);
+    arr=arr.concat(brr);
+   console.log(arr);
+  
 }
